8c3c03ccf6fe3959d03f2dd1d1ee0baea5c0a5fb
Kevin Leong <leongkevin@outlook.com>
2022-05-29 19:33:14 -0400

    Fixing randomizer

--- readline.js
+++ readline.js
@@ -16,50 +16,128 @@ console.log(`Welcome to Werewolf Tsukuyomi.`)
 const instructions = () => {
     console.log(`You are now bound to infinite Tsukuyomi until you complete your mission.`);
 }
-setTimeout(instructions, 1000);
+setTimeout(instructions, 500);
 
-// 3
-const chooseCharacter = () => {
-    rl.question(`Choose a character: Kent, Mong, or Cung. `, firstAnswer);
+
+
+// 3. Role Randomizer and declaration
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
-setTimeout(chooseCharacter, 4000);
 
-// 4
+const arrOfCharacters = [
+    Kent = {name: 'Kent'},
+    Mong = {name: 'Mong'},
+    Cung = {name: 'Cung'}
+];
+
+
+
+// Unquie combination of roles
+
+
+
+const length = arrOfCharacters.length;
+
+const combinFunc = (length) => {
+
+    if(length === 0) return 1
+
+    return length * combinFunc(length - 1);
+
+}
+
+const combinations = combinFunc(length);
+
+
+let yourRole;
+let secondRole;
+let thirdRole;
+const arrayOfRoles = [yourRole, secondRole, thirdRole];
+
+const chooseCharacter = () => {
+    console.log(arrOfCharacters)
+    rl.question(`Choose a character: `, firstAnswer);
+}
+setTimeout(chooseCharacter, 2500);
+
+
+// 4 Role Assignment
+
 let yourCharacter;
 
 const firstAnswer = (answer) => {
 
     yourCharacter = answer;
 
-    console.log(`Your secret role is ${firstRole}.`);
+    const role = getRandomInt(combinations);
+    if(role === 0) {
+        yourRole = 'Werewolf';
+        secondRole = 'Seer';
+        thirdRole = 'Villager';
+    } else if(role === 1) {
+        yourRole = 'Werewolf';
+        secondRole = 'Villager';
+        thirdRole = 'Seer';
+    } else if(role === 2) {
+        yourRole = 'Villager';
+        secondRole = 'Werewolf';
+        thirdRole = 'Seer';
+    } else if(role === 3) {
+        yourRole = 'Villager';
+        secondRole = 'Seer';
+        thirdRole = 'Werewolf';
+    } else if(role === 4) {
+        yourRole = 'Seer';
+        secondRole = 'Villager';
+        thirdRole = 'Werewolf';
+    } else if(role === 5) {
+        yourRole = 'Seer';
+        secondRole = 'Werewolf';
+        thirdRole = 'Villager';
+    }
 
-    const yourRole = () => {
+
+    for(let i = 0; i < arrOfCharacters.length; i++) {
+        currentObj = arrOfCharacters[i];
+        for(let key in currentObj) {
+            if(currentObj.name === answer) {
+                currentObj['role'] = yourRole;
+                }
+            }
+    }
+    console.log(arrOfCharacters)
+
+    console.log(`Your secret role is ${yourRole}.`);
+
+    const yourRoleOptions = () => {
 
         const firstQuestion = `${answer}, do you accept this mission? (Y/N)`
 
-        if(firstRole === 'Werewolf') {
+        if(yourRole === 'Werewolf') {
 
-            rl.question(`As ${answer}, ${firstRole} your mission is to kill the rest of the villagers before they find out who you are. ${firstQuestion} `, secondAnswerWerewolf)
+            rl.question(`As ${answer}, ${yourRole} your mission is to kill the rest of the villagers before they find out who you are. ${firstQuestion} `, secondAnswerWerewolf);
 
-        } else if(firstRole === 'Villager') {
+        } else if(yourRole === 'Villager') {
 
-            rl.question(`As ${answer}, ${firstRole} your missing is to identify the werewolf is before they kill you. ${firstQuestion} `, secondAnswerVillager);
+            rl.question(`As ${answer}, ${yourRole} your missing is to identify the werewolf is before they kill you. ${firstQuestion} `, secondAnswerVillager);
 
-        } else if(firstRole === 'Seer') {
+        } else if(yourRole === 'Seer') {
 
-            rl.question(`As ${answer}, ${firstRole} your mission is to alert the villagers about the identity of the werewolf without the werewolf finding out who you are. ${firstQuestion} `, secondAnswerSeer);
+            rl.question(`As ${answer}, ${yourRole} your mission is to alert the villagers about the identity of the werewolf without the werewolf finding out who you are. ${firstQuestion} `, secondAnswerSeer);
 
         }
     }
 
-    setTimeout(yourRole, 1000);
+    setTimeout(yourRoleOptions, 1000);
 }
 
 // 5
 
 const acceptMission = `There's no time to waste!`;
-const declineMissionNotWerewolf = `That's too bad...after the full moon, the werewolf eats all the villagers including you...`
-const declineMissionWerewolf = `The villagers gather their pitchforks against you...`
+const declineMissionNotWerewolf = `That's too bad...after the full moon, the werewolf eats all the villagers including you...`;
+const declineMissionWerewolf = `The villagers gather their pitchforks against you...`;
 
 const secondAnswerWerewolf = (answer) => {
     if(answer.toLowerCase() === 'Y'.toLowerCase()) {
@@ -91,101 +169,84 @@ const secondAnswerSeer = (answer) => {
 // 6
 
 const thirdAnswerWerewolf = (answer) => {
-    if(answer.toLowerCase() === yourCharacter.toLowerCase()) {
-        console.log(`Gameover, ${answer} commited senpuku!`);
+    if(answer.toLowerCase() === 'Kent'.toLowerCase) {
+        if(yourCharacter === 'Werewolf') {
+            console.log(`Gameover, ${answer} commited senpuku!`);
+        } else console.log(`Congratulations, ${answer} was a ${Kent.role}`);
         rl.close();
     } else if(answer.toLowerCase() === 'Mong'.toLowerCase()) {
-        console.log(`Congrats ${answer}! You win!`);
+        if(yourCharacter === 'Werewolf') {
+            console.log(`Gameover, ${answer} commited senpuku!`);
+        } else console.log(`Congratulations, ${answer} was a ${Mong.role}`);
         rl.close();
     } else if(answer.toLowerCase() === 'Cung'.toLowerCase()) {
-        console.log(`Congrats ${answer}! You win!`);
+        if(yourCharacter === 'Werewolf') {
+            console.log(`Gameover, ${answer} commited senpuku!`);
+        } else console.log(`Congratulations, ${answer} was a ${Cung.role}`);
         rl.close();
     }
 }
 
 const thirdAnswerVillager = (answer) => {
     if(answer.toLowerCase() === 'Kent'.toLowerCase()) {
-        console.log(`Congrats ${answer}! You win!`);
+        if(yourCharacter === 'Villager') {
+            console.log(`Gameover, ${answer}! You don't trust yourself, the villager!`);
+        } else console.log(`Congratulations, ${answer} was a ${Kent.role}`);
         rl.close();
     } else if(answer.toLowerCase() === yourCharacter.toLowerCase()) {
-        console.log(`You lose, ${answer}! You don't trust yourself, the villager!`);
+        if(yourCharacter === 'Villager') {
+            console.log(`Gameover, ${answer}! You don't trust yourself, the villager!`);
+        } else console.log(`Congratulations, ${answer} was a ${Mong.role}`);
         rl.close();
     } else if(answer.toLowerCase() === 'Cung'.toLowerCase()) {
-        console.log(`You lose, ${answer}! ${answer} is the Seer!`);
+        if(yourCharacter === 'Villager') {
+            console.log(`Gameover, ${answer}! You don't trust yourself, the villager!`);
+        } else console.log(`Congratulations, ${answer} was a ${Cung.role}`);
         rl.close();
     }
 }
 
 const thirdAnswerSeer = (answer) => {
     if(answer.toLowerCase() === 'Kent'.toLowerCase()) {
-        console.log(`Gameover, ${answer} kills you before you tell the other Villagers.`);
+        if(yourCharacter === 'Seer') {
+            console.log(`Gameover, ${answer}. You already know you're not the werewolf! Now you and other villagers are dead!`);
+        } else console.log(`Congratulations, ${answer} was a ${Kent.role}`);
         rl.close();
     } else if(answer.toLowerCase() === 'Mong'.toLowerCase()) {
-        console.log(`Congrats! ${answer} helps you kill Kent, the werewofl!`);
+        if(yourCharacter === 'Seer') {
+            console.log(`Gameover, ${answer}. You already know you're not the werewolf! Now you and other villagers are dead!`);
+        } else console.log(`Congratulations, ${answer} was a ${Mong.role}`);
         rl.close();
     } else if(answer.toLowerCase() === yourCharacter.toLowerCase()) {
-        console.log(`You already know you're not the werewolf! Now you and Mong are dead!`);
+        if(yourCharacter === 'Seer') {
+            console.log(`Gameover, ${answer}. You already know you're not the werewolf! Now you and other villagers are dead!`);
+        } else console.log(`Congratulations, ${answer} was a ${Cung.role}`);
         rl.close();
     }
 }
 
 
-// Below is not so random roles
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
+// if(yourCharacter === 'Kent') {
+//     yourCharacter.role = firstRole;
+//     Mong.role = secondRole;
+//     Cung.role = thirdRole;
+// } else if(yourCharacter === 'Mong') {
+//     yourCharacter.role = firstRole;
+//     Kent.role = secondRole;
+//     Cung.role = thirdRole;
+// } else if(yourCharacter === 'Cung') {
+//     yourCharacter.role = firstRole;
+//     Kent.role = secondRole;
+//     Mong.role = thirdRole;
+// }
 
-let secondRole;
-let thirdRole;
-
-const randomizeRole = (max) => {
-    const role = getRandomInt(max);
-    if(role === 0) {
-        secondRole = 'Seer'
-        thirdRole = 'Villager'
-        return 'Werewolf';
-    }
-    if(role === 1) {
-        secondRole = 'Villager'
-        thirdRole = 'Werewolf'
-        return 'Seer';
-    }
-    if(role === 2) {
-        secondRole = 'Werewolf'
-        thirdRole = 'Seer'
-        return 'Villager';
-    }
-}
-
-const max = 3;
-const firstRole = randomizeRole(max);
-
-const kent = {};
-const mong = {};
-const cung = {};
-
-if(yourCharacter === 'Kent') {
-    yourCharacter['role'] = firstRole;
-    mong['role'] = secondRole;
-    cung['role'] = thirdRole;
-} else if(yourCharacter === 'Mong') {
-    yourCharacter['role'] = firstRole;
-    kent['role'] = secondRole;
-    cung['role'] = thirdRole;
-} else if(yourCharacter === 'Cung') {
-    yourCharacter['role'] = firstRole;
-    kent['role'] = secondRole;
-    mong['role'] = thirdRole;
-}
-
-console.log(mong)
-
+// console.log(Kent)
 
 // console.log(firstRole)
 // console.log(secondRole)
 // console.log(thirdRole)
-// rl.close();
+
 
 // Below is an attempt at completely random roles
 
